cafe5021275c73fb694bc6521b5f5ad0acfdb8ae
Jmartw <146871238+Jmartw@users.noreply.github.com>
2023-11-09 15:58:26 -0500

    progress

--- src/main/java/com/pluralsight/ContractFileManager.java
+++ src/main/java/com/pluralsight/ContractFileManager.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
+import java.awt.datatransfer.StringSelection;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class ContractFileManager {
+
+    public void saveContract(Contract contract) {
+        try (BufferedWriter = new BufferedWriter(new FileWriter("contracts.csv, true "))) {
+            //Lease instance
+            if (contract instanceof LeaseContract){
+                writer.write(StringSelection.format)
+            }
+        }
+    }
 }
